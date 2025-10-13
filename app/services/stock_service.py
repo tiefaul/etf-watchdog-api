a6608c8f92644b810e5db7bb002d83520c9864cc
tiefaul <tylerfaulhaber@gmail.com>
2025-10-13 14:31:53 -0400

    Finished creating basic functions for stocks

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -18,6 +18,7 @@ class IYW:
         except ValueError as e:
             return f"Incorrect date format: {e}"
 
+        # Create a async request
         try:
             async with aiohttp.ClientSession() as request:
                 async with request.get(url=f'{url}/eod?symbol=IYW&date={date}&apikey={api_key}') as response:
@@ -33,6 +34,144 @@ class IYW:
                 output = await response.json()
                 return output["price"]
 
+class SMH:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=SMH&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=SMH&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
+class SPY:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=SPY&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=SPY&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
+class DRIV:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=DRIV&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=DRIV&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
+class IGV:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=IGV&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=IGV&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
+class CIBR:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=CIBR&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=CIBR&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
+class SHY:
+    async def get_price_by_date(self, url: str, date: str, api_key: str | None):
+        try:
+            datetime.strptime(date, "%Y-%m-%d") # Validate the date format
+            # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
+        except ValueError as e:
+            return f"Incorrect date format: {e}"
+
+        try:
+            async with aiohttp.ClientSession() as request:
+                async with request.get(url=f'{url}/eod?symbol=SHY&date={date}&apikey={api_key}') as response:
+                    output = await response.json()
+                    return output["close"]
+
+        except KeyError:
+            return f"This date does not appear in the stock data: {date}"
+
+    async def get_current_price(self, url: str, api_key: str | None):
+        async with aiohttp.ClientSession() as request:
+            async with request.get(url=f'{url}/price?symbol=SHY&apikey={api_key}') as response:
+                output = await response.json()
+                return output["price"]
+
 if __name__ == "__main__":
-    iyw = IYW()
-    print(asyncio.run(iyw.get_current_price(url=url, api_key=api_key)))
+    shy = SHY()
+    print(asyncio.run(shy.get_current_price(url=url, api_key=api_key)))
