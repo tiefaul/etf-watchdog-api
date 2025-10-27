114876882736f5614bc7a55e3beca1a66cbaa930
tiefaul <tylerfaulhaber@gmail.com>
2025-10-26 20:54:35 -0400

    updated stock router to recieve query of stock price by date

--- app/routers/stocks.py
+++ app/routers/stocks.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Query, HTTPException
+from fastapi import APIRouter, Query, HTTPException, Path
 from ..services.stock_service import Stock
 from typing import Annotated
+import datetime
+import os
+
+url = "https://api.twelvedata.com"
+api_key = os.getenv("API_KEY")
 
 router = APIRouter(
         prefix="/api/stocks",
@@ -11,14 +16,29 @@ router = APIRouter(
         )
 
 @router.get("/")
-async def get_all_stocks(symbol: Annotated[str | None, Query(description="Retrieve stock by the stock's ticker symbol", min_length=1, max_length=5)] = None):
+async def get_all_stocks():
     stock = Stock()
     stocks_dict = await stock.get_stocks()
-    # If query is given
-    if symbol is not None:
-        if symbol in stocks_dict["stocks"]:
-            return symbol
-        else:
-            raise HTTPException(status_code=404, detail=f"{symbol} symbol not found.")
-    # Else return all stocks
     return stocks_dict
+
+@router.get("/{symbol}")
+async def get_stock(
+        symbol: Annotated[str | None, Path(description="Get stock by ticker symbol.", min_length=1, max_length=5)],
+        price: Annotated[bool | None, Query(description="Show the current trading price.")] = None,
+        date: Annotated[str | None, Query(description="Retrieve price by date. Must be in 'year-month-day' format.")] = None
+        ):
+
+    stock = Stock()
+    stock_dict = await stock.get_stocks()
+    if symbol in stock_dict["stocks"]:
+        results = {"symbol": symbol}
+        if price is True:
+            stock_price = await stock.get_current_price(url=url, symbol=symbol, api_key=api_key) #type: ignore
+            results.update({"price_current": stock_price})
+        if date:
+            stock_price_by_date = await stock.get_price_by_date(url=url, symbol=symbol, date=date, api_key=api_key) #type: ignore
+            results.update({f"price_{date}": stock_price_by_date})
+
+        return results
+    else:
+        raise HTTPException(status_code=404, detail="404 not found")
--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -3,22 +3,25 @@ import os
 from datetime import datetime
 import asyncio
 import aiohttp
+from fastapi import HTTPException
 
 load_dotenv()
 
 class Stock:
+    # Retrieve all monitored stocks
     async def get_stocks(self):
         return {"stocks": ["SHY", "CIBR", "IGV", "DRIV", "SPY", "SMH", "IYW"]}
 
+    # Get price for stock by date
     async def get_price_by_date(self, url: str, date: str, symbol: str, api_key: str | None):
         stock = await self.get_stocks()
         if symbol not in stock["stocks"]:
-            raise KeyError(f"Symbol: {symbol} not in the database.")
+            raise HTTPException(status_code=404, detail=f"Symbol: {symbol} not in the database.")
         try:
             datetime.strptime(date, "%Y-%m-%d") # Validate the date format
             # today_date = str(datetime.now().strftime("%Y-%m-%d")) # Outputs a string
         except ValueError as e:
-            return f"Incorrect date format: {e}"
+            raise HTTPException(status_code=404, detail= f"Incorrect date format: {e}")
 
         # Create a async request
         try:
@@ -27,15 +30,18 @@ class Stock:
                     output = await response.json()
                     return output["close"]
 
-        except KeyError:
-            return f"This date does not appear in the stock data: {date}"
+        except KeyError as e:
+            raise HTTPException(status_code=404, detail= f"{date} does not appear in the stock data: {e}")
 
+    # Get current price of stock
     async def get_current_price(self, url: str, symbol: str, api_key: str | None):
-        stock = await self.get_stocks()
+        stock = await self.get_stocks() # Get all stocks in the database and raise an error if it doesn't exist
         if symbol not in stock["stocks"]:
-            raise KeyError(f"{symbol} not in the database.")
+            raise HTTPException(status_code=404, detail=f"Symbol: {symbol} not in the database.")
+
+        # Create async request
         async with aiohttp.ClientSession() as request:
-            async with request.get(url=f'{url}/price?symbol=IYW&apikey={api_key}') as response:
+            async with request.get(url=f'{url}/price?symbol={symbol}&apikey={api_key}') as response:
                 output = await response.json()
                 return output["price"]
 
