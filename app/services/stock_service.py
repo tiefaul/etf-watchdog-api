d15ae51d845a4228c7d89e40f10cf35212b7821d
tiefaul <tylerfaulhaber@gmail.com>
2025-12-14 20:06:36 -0500

    start of refactor

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -9,22 +9,32 @@ setup_logging()
 
 class Stock:
     # Create handler function for stock api calls
-    async def stock_data_requests(self, symbol: str, date: str | None = None, api_key: str | None = None):
-            async with aiohttp.ClientSession(base_url="https://api.twelvedata.com") as request:
-                parameters = {"symbol": symbol, "apikey": api_key}
-                if date:
-                    parameters["date"] = date
-                    logger.debug(f"Running api call to get {symbol} price by date.")
+    async def stock_data_requests(self,
+                                  symbol: str,
+                                  api_key: str,
+                                  price: bool | None = False,
+                                  date: str | None = None,
+                                  ):
 
-                    try:
-                        async with request.get('/eod', params=parameters) as response:
-                            close_date = await response.json()
-                            logger.info(f"Successfully obtain {symbol} price on {date}.")
-                            return close_date['close']
-                    except KeyError as e:
-                        raise HTTPException(status_code=404, detail=f"{date} does not appear in the stock data: {e}")
+        stock_representation = {}
 
-                logger.debug(f"Running api call to get {symbol} current stock price.")
+        async with aiohttp.ClientSession(base_url="https://api.twelvedata.com") as request:
+            parameters = {"symbol": symbol, "apikey": api_key}
+            if date:
+                parameters["date"] = date
+                logger.debug(f"Running api call to get {symbol} price by date.")
+                try:
+                    # Get end of day price by date
+                    async with request.get('/eod', params=parameters) as response:
+                        close_date = await response.json()
+                        logger.info(f"Successfully obtain {symbol} price on {date}.")
+                        return close_date['close']
+                except KeyError as e:
+                    raise HTTPException(status_code=404, detail=f"{date} does not appear in the stock data: {e}")
+
+            logger.debug(f"Running api call to get {symbol} current stock price.")
+            # Get the current price
+            if price is True:
                 async with request.get('/price', params=parameters) as response:
                     logger.info(f"Successfully obtained {symbol} current stock price.")
                     current_price = await response.json()
@@ -38,7 +48,7 @@ class Stock:
         return stocks
 
     # Get price for stock by date
-    async def get_price_by_date(self, date: str, symbol: str, api_key: str | None):
+    async def get_price_by_date(self, date: str, symbol: str, api_key: str):
         # Get all stocks in the database and raise an error if symbol does not exist
         stock = await self.get_stocks()
         if symbol not in stock["stocks"]:
@@ -51,26 +61,23 @@ class Stock:
             logger.error(f"Incorrect date format ({date}): {e}", exc_info=False)
 
         # Make request to the api
-        logger.debug("Running api call to get stock price by date.")
+        logger.debug("Running get_price_by_date function.")
         output = await self.stock_data_requests(symbol=symbol, date=date, api_key=api_key)
         logger.debug("Successfully ran get_price_by_date function.")
         return output
 
     # Get current price of stock
-    async def get_current_price(self, symbol: str, api_key: str | None):
+    async def get_current_price(self, symbol: str, api_key: str):
         # Get all stocks in the database and raise an error if symbol does not doesn't exist
         stock = await self.get_stocks()
         if symbol not in stock["stocks"]:
             logger.error(f"Symbol: {symbol} not in the database.")
 
         # Make request to the api
-        logger.debug("Running api call to get current stock price.")
-        output = await self.stock_data_requests(symbol=symbol, api_key=api_key)
+        logger.debug("Running get_current_price function.")
+        output = await self.stock_data_requests(symbol=symbol, price=True, api_key=api_key)
         logger.debug("Successfully ran get_current_price function.")
         return output
-
-    async def get_stock_name(self):
-        pass
 
     async def get_news(self,):
         pass
