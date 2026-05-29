6ce2d2acd2d28a41132eddcf3e7ad746fce6b391
tiefaul <tylerfaulhaber@gmail.com>
2026-05-29 16:30:04 -0400

    added a unique constraint to the stock_id and price_date columns for stockprice

--- app/internal/models.py
+++ app/internal/models.py
@@ -56,7 +56,7 @@ class StockPrice(SQLModel, table=True):
 class NewsArticle(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
     title: str | None = None
-    url: str = Field(unique=True)
+    url: str
     summary: str | None = None
     created_at: datetime | None = Field(
         default=None,
