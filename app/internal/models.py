1c705c0575859cac200e646faff3538085c9e4b1
tiefaul <tylerfaulhaber@gmail.com>
2026-05-29 16:30:04 -0400

    added a unique constraint to the stock_id and price_date columns for stockprice

--- app/internal/models.py
+++ app/internal/models.py
@@ -1,23 +1,26 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, UniqueConstraint
 
 
 class Stock(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
-    ticker_symbol: str
+    ticker_symbol: str = Field(unique=True, index=True)
     company_name: str | None = None
     currency: str | None = None
 
 
 class StockPrice(SQLModel, table=True):
+    __table_args__ = (
+            UniqueConstraint("stock_id", "price_date"),
+            )
     id: int | None = Field(primary_key=True, default=None)
     stock_id: int = Field(foreign_key="stock.id")
-    price_date: str = Field(unique=True, index=True)
+    price_date: str = Field(index=True)
     close_price: int | None = None
 
 
 class NewsArticles(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
     title: str | None = None
-    url: str = Field(unique=True)
+    url: str
     summary: str | None = None
 
