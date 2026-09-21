97f2d33a7caaa7b586d6af923c685c4cd5d42b36
tiefaul <tylerfaulhaber@gmail.com>
2026-09-21 14:13:36 -0400

    removed header component and just left it in the layout file.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-  import { Header } from '$lib/components';
+  import { ModalButton } from "$lib/components";
+
 	let { children } = $props();
+  let showModal: boolean = $state(false);
 </script>
 
 <svelte:head>
@@ -12,6 +14,39 @@
   </style>
 </svelte:head>
 
-<Header />
+
+<header class='header-flex-container'>
+  <h1>ETF Watchdog</h1>
+  <button onclick={() => (showModal = true)}>+ Add Stock</button>
+</header>
+
+<ModalButton bind:showModal />
+
+<div>
+  <p>Track and manage your investments</p>
+  <hr />
+</div>
+
+
+<style>
+  .header-flex-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-left: 20px;
+  }
+  .header-flex-container h1 {
+    margin-bottom: 0px;
+  }
+  p {
+    margin-top: 0px;
+    margin-left: 20px;
+  }
+  hr {
+    margin-bottom: 15px;
+  }
+</style>
+
 
 {@render children()}
